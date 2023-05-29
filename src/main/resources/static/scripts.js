c35ce0adc9cbf79362768bc699de076a19c17cab
Jake Hennett <jake.hennett@gmail.com>
2023-05-28 20:28:24 -0400

    Create function to get access token and html button to test

--- src/main/resources/static/scripts.js
+++ src/main/resources/static/scripts.js
@@ -10,11 +10,59 @@ function submit() {
 
 async function getAccessToken() {
     let accessToken = ""
-    //Create an HTTP request that matches the following cURL
-    //     curl --location 'https://test.api.amadeus.com/v1/security/oauth2/token' \
-    // --header 'Content-Type: application/x-www-form-urlencoded' \
-    // --data-urlencode 'grant_type=client_credentials' \
-    // --data-urlencode 'client_id=4u3S14ZTnv0G75lnMzRvz9CDNQ0aPGMt' \
-    // --data-urlencode 'client_secret=kXzFmTwO4YphklII'
+    let url = "https://test.api.amadeus.com/v1/security/oauth2/token"
+
+    var details = {
+        'client_id': '4u3S14ZTnv0G75lnMzRvz9CDNQ0aPGMt',
+        'client_secret': 'kXzFmTwO4YphklII',
+        'grant_type': 'client_credentials'
+    };
+
+    var formBody = [];
+    for (var property in details) {
+      var encodedKey = encodeURIComponent(property);
+      var encodedValue = encodeURIComponent(details[property]);
+      formBody.push(encodedKey + "=" + encodedValue);
+    }
+    formBody = formBody.join("&");
+
+    try {
+        const config = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+            },
+            body: formBody
+        }
+        const response = await fetch(url, config)
+        if (response.ok) {
+            accessToken = response
+        } else {
+            //
+        }
+    } catch (error) {
+
+    }
+
     return accessToken
+}
+
+//Example HTTP request function
+async function getForecast(latitude, longitude) {
+    let forecastRequestString = "http://localhost:8080/weather/forecast/" + latitude + "/" + longitude;
+
+    let headers = new Headers([
+        ['Content-Type', 'application/json'],
+        ['Accept', 'application/json']
+    ]);
+
+    let request = new Request(forecastRequestString, {
+        method: 'GET',
+        headers: headers
+    });
+
+    let result = await fetch(request);
+    let response = await result.json();
+    let forecast = response.properties.periods[0].detailedForecast;
+    alert(forecast);
 }
