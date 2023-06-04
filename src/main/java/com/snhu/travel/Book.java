bb4bc0eaad8c4d1493f7fe11849742a8e9a5cad4
Jake Hennett <jake.hennett@gmail.com>
2023-06-04 17:41:56 -0400

    That tutorial was just returning XML through an API response. Next.

--- src/main/java/com/snhu/travel/Book.java
+++ src/main/java/com/snhu/travel/Book.java
@@ -1,10 +1,14 @@
 package com.snhu.travel;
+//source: https://learnjava.co.in/how-to-create-a-spring-boot-rest-service-with-xml-output/
 
 public class Book {
 
-  private int id;
-  private String name;
-  private String author;
+    private int id;
+    private String name;
+    private String author;
 
-        //constructor, getters and setters
+    // constructor, getters and setters
+
+    public Book(int i, String string, String string2) {
+    }
 }
