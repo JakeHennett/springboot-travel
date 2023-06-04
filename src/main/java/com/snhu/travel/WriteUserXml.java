602ad1ba047057cb76fd4993aed936021ba87b5d
Jake Hennett <jake.hennett@gmail.com>
2023-06-04 19:54:42 -0400

    Create xml without data

---
+++ src/main/java/com/snhu/travel/WriteUserXml.java
@@ -0,0 +1,61 @@
+package com.snhu.travel;
+//my variation on writexmldom1
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class WriteUserXml {
+
+    public static void main(String[] args)
+            throws ParserConfigurationException, TransformerException {
+
+        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+
+        // root elements
+        Document doc = docBuilder.newDocument();
+        Element rootElement = doc.createElement("users");
+        doc.appendChild(rootElement);
+
+        Element userElement = doc.createElement("user");
+        rootElement.appendChild(userElement);
+
+        userElement.appendChild(doc.createElement("username"));
+        userElement.appendChild(doc.createElement("password"));
+
+        // ...create XML elements, and others...
+
+        // write dom document to a file
+        try (FileOutputStream output = new FileOutputStream("c:\\test\\users.xml")) {
+            writeXml(doc, output);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    // write doc to output stream
+    private static void writeXml(Document doc,
+            OutputStream output)
+            throws TransformerException {
+
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        DOMSource source = new DOMSource(doc);
+        StreamResult result = new StreamResult(output);
+
+        transformer.transform(source, result);
+
+    }
+}
