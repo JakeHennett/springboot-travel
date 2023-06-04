cc501ae7b6f51c91e9f379e22fed52258b70753e
Jake Hennett <jake.hennett@gmail.com>
2023-06-04 19:38:40 -0400

    Created a User object type

---
+++ src/main/java/com/snhu/travel/WriteXmlDom1.java
@@ -0,0 +1,58 @@
+package com.snhu.travel;
+//source https://mkyong.com/java/how-to-create-xml-file-in-java-dom/
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class WriteXmlDom1 {
+
+    public static void main(String[] args)
+            throws ParserConfigurationException, TransformerException {
+
+        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+
+        // root elements
+        Document doc = docBuilder.newDocument();
+        Element rootElement = doc.createElement("company");
+        doc.appendChild(rootElement);
+
+        doc.createElement("staff");
+        rootElement.appendChild(doc.createElement("staff"));
+
+        // ...create XML elements, and others...
+
+        // write dom document to a file
+        try (FileOutputStream output = new FileOutputStream("c:\\test\\staff-dom.xml")) {
+            writeXml(doc, output);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    // write doc to output stream
+    private static void writeXml(Document doc,
+            OutputStream output)
+            throws TransformerException {
+
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        DOMSource source = new DOMSource(doc);
+        StreamResult result = new StreamResult(output);
+
+        transformer.transform(source, result);
+
+    }
+}
