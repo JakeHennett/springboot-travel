6c621629d73e5c3ff590361d435b1fcd2fe679b5
Jake Hennett <jake.hennett@gmail.com>
2023-06-04 18:13:12 -0400

    Trying XML i/o again

---
+++ src/main/java/com/snhu/travel/XMLTest.java
@@ -0,0 +1,12 @@
+package com.snhu.travel;
+
+import javax.xml.parsers.*;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.*;
+import javax.xml.transform.stream.*;
+import org.xml.sax.*;
+import org.w3c.dom.*;
+
+public class XMLTest {
+    
+}
