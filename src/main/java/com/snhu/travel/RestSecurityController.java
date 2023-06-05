ab019769ab987bb78cc3585e12b0eeae8c18b368
Jake Hennett <jake.hennett@gmail.com>
2023-06-04 23:11:05 -0400

    Basic boilerplate for register new user

--- src/main/java/com/snhu/travel/RestSecurityController.java
+++ src/main/java/com/snhu/travel/RestSecurityController.java
@@ -89,103 +89,34 @@ public class RestSecurityController {
         return userList;
     }
 
-    //http://localhost:8080/security/validate?uname=default&pass=password
+    // http://localhost:8080/security/validate?uname=default&pass=password
     @GetMapping(value = "/security/validate")
     private boolean validateUser(@RequestParam String uname, @RequestParam String pass) {
-        boolean response = false;
         List<User> userList = getAllUsers();
         User validate = new User(uname, pass);
-        // boolean response = userList.contains(validate);
-        System.out.println(validate.getUsername());
-        System.out.println(validate.getPassword());
 
-        for (User checkUser : userList)
-        {
-            System.out.println(checkUser.getUsername());
-            System.out.println(checkUser.getPassword());
-
-            if(validate.getUsername().equals(checkUser.getUsername()))
-            {
-                if(validate.getPassword().equals(checkUser.getPassword()))
-                {
-                    response = true;
+        for (User checkUser : userList) {
+            if (validate.getUsername().equals(checkUser.getUsername())) {
+                if (validate.getPassword().equals(checkUser.getPassword())) {
+                    return true;
                 }
             }
         }
-
-
-        return response;
+        return false;
     }
-    // @GetMapping(value = "/callclienthello")
-    // private String getHelloClient() {
-    // String uri = "http://localhost:8080/test";
-    // RestTemplate restTemplate = new RestTemplate();
-    // String result = restTemplate.getForObject(uri, String.class);
-    // return result;
-    // }
 
-    // @GetMapping(value = "/scryfall")
-    // private String getScryfallRandom() {
-    // String uri = "https://api.scryfall.com/cards/random";
-    // RestTemplate restTemplate = new RestTemplate();
-    // String result = restTemplate.getForObject(uri, String.class);
-    // return result;
-    // }
+    // http://localhost:8080/security/validate?uname=default&pass=password
+    @GetMapping(value = "/security/register")
+    private void registerUser(@RequestParam String uname, @RequestParam String pass) {
+        //TODO: Add some conditions at this level. No duplicate users, etc.
+        //TODO: Make it a POST and not a GET
+        addUser(uname, pass);
+    }
 
-    // //http://localhost:8080/weather/points/31/-82
-    // @GetMapping(value = "/weather/points/{latitude}/{longitude}")
-    // private String getPointsByCoordinates(@PathVariable Long latitude,
-    // @PathVariable Long longitude) {
-    // String uri = "https://api.weather.gov/points/" + latitude + "," + longitude;
-    // RestTemplate restTemplate = new RestTemplate();
-    // String result = restTemplate.getForObject(uri, String.class);
-    // return result;
-    // }
-
-    // //http://localhost:8080/weather/forecast/31/-82
-    // @GetMapping(value = "/weather/forecast/{latitude}/{longitude}")
-    // private String getForecastByCoordinates(@PathVariable Long latitude,
-    // @PathVariable Long longitude) {
-    // RestTemplate restTemplate = new RestTemplate();
-
-    // //points - https://api.weather.gov/points/35,-82
-    // String pointsUri = "https://api.weather.gov/points/" + latitude + "," +
-    // longitude;
-    // String pointsStringResult = restTemplate.getForObject(pointsUri,
-    // String.class);
-
-    // // "forecast": "https://api.weather.gov/gridpoints/JAX/50,93/forecast"
-    // // Use indexOf and substring to isolate the forecast url for the provided GPS
-    // coordinates.
-    // int urlLocationStart =
-    // pointsStringResult.indexOf("https://api.weather.gov/gridpoints");
-    // int urlLocationEnd = pointsStringResult.indexOf("forecast",
-    // urlLocationStart);
-    // String extractedURL = pointsStringResult.substring(urlLocationStart,
-    // urlLocationEnd+8);
-    // String forecastResult = restTemplate.getForObject(extractedURL,
-    // String.class);
-    // //http://localhost:8080/weather/forecast/35/-82 =
-    // https://api.weather.gov/gridpoints/GSP/78,51/forecast
-    // //http://localhost:8080/weather/forecast/31/-82 =
-    // https://api.weather.gov/gridpoints/JAX/50,93/forecast
-    // //http://localhost:8080/weather/forecast/30/-85 =
-    // https://api.weather.gov/gridpoints/TAE/58,65/forecast
-
-    // // This would be dynamic and more reliable, but we aren't going to bother
-    // with JSON manipulation right now.
-    // // //forecast - https://api.weather.gov/gridpoints/{office}/{grid X},{grid
-    // Y}/forecast
-    // // String office = "";
-    // // //pointsResult.office;
-    // // String gridX = "";
-    // // String gridY = "";
-    // // String forecastUri = "https://api.weather.gov/gridpoints/" + office + "/"
-    // + gridX + "," + gridY + "/forecast";
-    // // Forecast forecastResult = restTemplate.getForObject(forecastUri,
-    // Forecast.class);
-
-    // String result = forecastResult;
-    // return result;
-    // }
+    private void addUser(String uname, String pass) {
+        User newUser = new User(uname, pass);
+        List<User> userList = getAllUsers();
+        userList.add(newUser);
+        //TODO: write to xml file here.
+    }
 }
