ae3c16d2996c6f324d62cba008127f77c262d652
Jake Hennett <jake.hennett@gmail.com>
2023-06-10 23:48:08 -0400

    Added controller routing for custom login page

--- src/main/java/com/snhu/travel/HtmlController.java
+++ src/main/java/com/snhu/travel/HtmlController.java
@@ -1,8 +1,25 @@
 package com.snhu.travel;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+@Controller
+@RequestMapping(value = "/customlogin")
 public class HtmlController {
+
+    //Mimicked from https://www.baeldung.com/spring-controllers
+    @GetMapping
+    public ModelAndView getLoginPage() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("customlogin");
+        mv.getModel().put("data", "Login page");
+
+        return mv;
+    }
+
     public String index(Model model){
         return "index";
         //Get more details here https://www.youtube.com/watch?v=cLJ7pSmOdXA&ab_channel=SNHUMedia
