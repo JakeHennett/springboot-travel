7736e5b90d0c34a667640ee36badef7e49fff761
Jake Hennett <jake.hennett@gmail.com>
2023-06-18 13:36:42 -0400

    Create two valid pass/fail tests for the rest test endpoint

--- src/test/java/com/snhu/travel/TravelApplicationTests.java
+++ src/test/java/com/snhu/travel/TravelApplicationTests.java
@@ -2,18 +2,34 @@ package com.snhu.travel;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class TravelApplicationTests {
 	
+
+	@Value(value="${local.server.port}")
+	private int port;
+
 	@Autowired
-	private HtmlController controller;
+	private TestRestTemplate restTemplate;
 
 	@Test
-	public void contextLoads() throws Exception {
-		assertThat(controller).isNotNull();
+	public void testEndpointShouldReturnSuccessResponseCode() throws Exception {
+		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/test",
+				String.class)).contains("test string output");
+	}
+
+	@Test	//This test should fail. Uncomment it to verify that the tests are indeed running properly.
+	public void avoidFalsePass() throws Exception {
+		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/test",
+				String.class)).contains("please fail");
 	}
 
 }
